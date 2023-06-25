52c7fa86601096bb3c0d6d3385518bb54858a4bc
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2023-06-25 18:06:16 +0700

    dj classification

--- aets1/app/__init__.py
+++ aets1/app/__init__.py
@@ -1,5 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
+
+import pandas as pd
+import numpy as np
+from scipy.fftpack import fft
+from scipy import signal
+from scipy.io import wavfile
+import librosa
+from pydub import AudioSegment
+from datetime import datetime
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import IPython.display as ipd
+import librosa.display
+from itertools import cycle
+
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
+import os
+import random
+from glob import glob
+import warnings
+import soundfile as sf
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv1D, Input, MaxPooling1D
+from tensorflow.keras.models import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import ImageDataGenerator
 
 app = Flask(__name__)
+
+# load model for prediction
+modelcnn = load_model("D:/Adria Tisnawati A/Matkul Polindra/Semester6/Automatic_Exam_Trust_Score/aets1/app/best_model.hdf5")
+
+
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'wav'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 from app import routes 
--- aets1/app/routes.py
+++ aets1/app/routes.py
@@ -1,9 +1,61 @@
 from app import app
 from flask import render_template
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+@app.route("/classification", methods = ['GET', 'POST'])
+def classification():
+	return render_template("classifications.html")
+
+@app.route('/submit', methods=['POST'])
+def predict_sound():
+    if 'file' not in request.files:
+        resp = jsonify({'message': 'No sound in the request'})
+        resp.status_code = 400
+        return resp
+    files = request.files.getlist('file')
+    filename = "temp_sound.wav"
+    errors = {}
+    success = False
+    for file in files:
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            success = True
+        else:
+            errors["message"] = 'File type of {} is not allowed'.format(file.filename)
+
+    if not success:
+        resp = jsonify(errors)
+        resp.status_code = 400
+        return resp
+    sound_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    # Load sound file and extract features
+    data, sr = sf.read(sound_path)
+    
+    data, sr = sf.read(sound_path)
+    # Perform feature extraction on the sound file (e.g., using librosa)
+    # Replace the following code with your feature extraction code
+    # features = extract_features(data, sr)
+    features = np.random.rand(128)  # Dummy features for illustration
+
+    # Reshape features for prediction
+    features = np.expand_dims(features, axis=0)
+
+    # Predict
+    prediction_array_cnn = modelcnn.predict(features)
+
+    # Prepare API response
+    class_names = ['With Pest', 'Without Pest']
+
+    return render_template("classifications.html", predictioncnn=class_names[np.argmax(prediction_array_cnn)],
+                           confidencecnn='{:.2f}%'.format(100 * np.max(prediction_array_cnn)))
+
+
 if __name__ == '__main__':
+    # app.debug = True
     app.run(debug = True)
+    
